18825f45abaf16fea608e93bb31d812ddaba0dac
unknown <degamiesign@gmail.com>
2026-09-04 18:21:41 +0530

    GameView#impl

--- app/src/main/java/com/example/cargame/View/GameThread.java
+++ app/src/main/java/com/example/cargame/View/GameThread.java
@@ -1,6 +1,8 @@
+//WID(4/9/2026)(Sarthak Mittal)(DegamieSign)(GameThread)#impl.1.1.1.1.1
 package com.example.cargame.View;
 
 public class GameThread {
+    public GameView gameView;
     boolean b=false;
     public void setRunning(boolean b) {
         this.b = b;
