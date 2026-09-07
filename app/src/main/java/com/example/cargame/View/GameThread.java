e183c0ed5844d4c3cffdab8b1be7857513e34376
unknown <degamiesign@gmail.com>
2026-09-07 07:44:29 +0530

    binding#fps

--- app/src/main/java/com/example/cargame/View/GameThread.java
+++ app/src/main/java/com/example/cargame/View/GameThread.java
@@ -1,8 +1,11 @@
-//WID(6/9/2026)(Sarthak Mittal)(DegamieSign)(GameThread)#impl.1.1.1.1.1
+//WID(7/9/2026)(Sarthak Mittal)(DegamieSign)(GameThread)#impl.1.1.1.1.1
 package com.example.cargame.View;
 
 public class GameThread {
-    private static final int MAX_FPS = 60;
+    void setMAX_FPS(int  MAX_FPS){
+        this.MAX_FPS=MAX_FPS;
+    }
+    private static int MAX_FPS = 60;
     public GameView gameView;
     boolean b=false;
     public void setRunning(boolean b) {
