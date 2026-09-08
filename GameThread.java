c048f0a47c851c30739aa58496d9e4caf16a0cfc
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-09-08 11:38:36 +0530

    Add files via upload

---
+++ GameThread.java
@@ -0,0 +1,51 @@
+//WID(8/9/2026)(Sarthak Mittal)(DegamieSign)(GameThread)#impl.1.1.1.1.1.1.1
+package com.example.cargame.View;
+
+public class GameThread {
+    void setgameview(GameView gameView){
+        this.gameView=gameView;
+    }
+    void setMAX_FPS(int  MAX_FPS){
+        this.MAX_FPS=MAX_FPS;
+    }
+    private static int MAX_FPS = 60;
+    public GameView gameView;
+    boolean b=false;
+    public void setRunning(boolean b) {
+        this.b = b;
+    }
+
+    public void start() {
+        this.b = true;
+    }
+    @Override
+    public void run(){
+        long targetTime = 1000/MAX_FPS;
+        while(running){
+            long startTime=System.currentTimeMillis();
+            Canvas canvas=null;
+            try{
+                canvas=surfaceHolders.lockedCanvas();
+                synchronized (surfaceHolders){
+                    gameView.update();
+                    gameView.draw();
+
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        } finally {
+            if (canvas != null) {
+                try {
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
+}
