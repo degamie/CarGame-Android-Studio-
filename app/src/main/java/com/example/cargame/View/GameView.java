618c3102132f0a3cf52513d7ecdd1521737a5162
unknown <degamiesign@gmail.com>
2026-09-14 21:19:52 +0530

    Existingscore

--- app/src/main/java/com/example/cargame/View/GameView.java
+++ app/src/main/java/com/example/cargame/View/GameView.java
@@ -1,4 +1,4 @@
-//WID(14/9/2026)(Sarthak Mittal)(DegamieSign)(GameView)#impl.1.1.1.1.1/1.11.1.1.1
+//WID(14/9/2026)(Sarthak Mittal)(DegamieSign)(GameView)#impl.1.1.1.1.1/1.11.1.1.1.1
 package com.example.cargame.View;
 
 import android.graphics.RectF;
@@ -17,6 +17,10 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class GameView extends SurfaceView implements SurfaceHolders.CallBack{
+    void existsByscore(int score){
+        if(score!=0)getscore(score);
+        else getscore(0);
+    }
     void updateByscore(int score){
         getByscore(score)+setscore(score)+1;
     }
