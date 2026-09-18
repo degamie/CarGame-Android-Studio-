8f8d22b87ff405382367e11588e9fe7506253dd5
unknown <degamiesign@gmail.com>
2026-09-18 21:10:59 +0530

    binding#paint

--- app/src/main/java/com/example/cargame/View/GameView.java
+++ app/src/main/java/com/example/cargame/View/GameView.java
@@ -1,4 +1,4 @@
-//WID(18/9/2026)(Sarthak Mittal(DegamieSign)(GameView)
+//WID(18/9/2026)(Sarthak Mittal(DegamieSign)(GameView)(binding Paint)
 package com.example.cargame.View;
 
 import android.content.Context;
@@ -23,6 +23,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
     private GameThread gameThread;
     private final Paint paint;
+    void setpaint(Paint paint){this.paint=paint;}
 
     private final Random random;
 
