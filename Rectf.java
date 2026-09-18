21686f1e039849d1af231e721b01b299e30b7f2c
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-09-18 17:50:22 +0530

    Add files via upload

---
+++ Rectf.java
@@ -0,0 +1,32 @@
+//WID(18/9/2026)(Sarthak Mittal)(DegamieSign)(Rectf)#impl.1.1.1.1.1.1..11/1.1
+package com.example.cargame.View;
+
+import android.app.GameManager;
+import android.graphics.RectF;
+import android.view.Window;
+
+public class Rectf {
+    Rectf rectf;
+    void setrectf(Rectf rectf){this.rectf=rectf;}
+    Rectf getRectf(Rectf rectf){
+        return rectf;
+    }
+    Window getWindow(Window window){return window;}
+    void setwindow(Window window){this.window=window;}
+    public Window window;
+    public GameManager gameManager;
+
+    public GameManager getGameManager() {
+        return gameManager;
+    }
+
+    public void setGameManager(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+    public Rectf(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+
+}
+   // void updateBygameManager(GameManager gameManager) throws {getGameManager()+setGameManager(gameManager)+1;}
+
