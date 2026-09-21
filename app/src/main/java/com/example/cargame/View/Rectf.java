112bea72ffeffc1d6482f1a643c7796533fdc0b7
unknown <degamiesign@gmail.com>
2026-09-21 12:26:50 +0530

    binding#mainactivity

--- app/src/main/java/com/example/cargame/View/Rectf.java
+++ app/src/main/java/com/example/cargame/View/Rectf.java
@@ -1,4 +1,4 @@
-//WID(20/9/2026)(Sarthak Mittal)(DegamieSign)(Rectf)#impl.1.1.1.1.1.1..11/1.1.1.1.1
+//WID(21/9/2026)(Sarthak Mittal)(DegamieSign)(Rectf)#impl.1.1.1.1.1.1..11/1.1.1.1.1
 package com.example.cargame.View;
 
 import android.app.GameManager;
@@ -9,6 +9,10 @@ import com.example.cargame.MainActivity;
 
 public class Rectf extends MainActivity {
     MainActivity mainApplication;
+    void setmainApplication(MainActivity mainApplication){
+        this.mainApplication=mainApplication;
+    }
+
      void updateBygameManager(GameManager gameManager) throws {getGameManager(gameManager)+setGameManager(gameManager)+1;}
     void updateByrectf(Rectf rectf){getRectf(rectf)+setrectf(rectf)+1;}//updating rectf in Gameapp
     Rectf rectf;
