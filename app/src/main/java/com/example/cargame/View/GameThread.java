eae96c15c3966cd177a5450ccdecc2c54c5739c1
unknown <degamiesign@gmail.com>
2026-09-25 22:44:12 +0530

    checking#canvas

--- app/src/main/java/com/example/cargame/View/GameThread.java
+++ app/src/main/java/com/example/cargame/View/GameThread.java
@@ -1,10 +1,14 @@
-//WID(23/9/2026)(Sarthak Mittal)(DegamieSign)(GameThread)(binding#canvas)#1.1/1.1.1
+//WID(25/9/2026)(Sarthak Mittal)(DegamieSign)(GameThread)(binding#canvas)#1.1/1.1.1.1
 package com.example.cargame.View;
 
 import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
 public class GameThread {
+     void existsBycanvas(Canvas canvas){
+         if(canvas.save()!=null)getcanvas(canvas);
+         else getcanvas(null);
+     }
     void updateBycanvas(Canvas canvas){
         getcanvas(canvas)+setcanvas(canvas)+1;
     }
